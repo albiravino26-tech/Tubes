f6565fb3a25025a5b4ab2a6c4aeb3a2a06ebef88
Fabian Albira Vino Angada <albiravino26@gmail.com>
2026-06-11 10:08:06 +0700

    Refactor menu and add mental health options

--- ClearMind.go
+++ ClearMind.go
@@ -19,14 +19,23 @@ func main() {
 }
 func menuUtama(A *tabtugas){
 	var pilih int
-	fmt.Println("====SELAMAT DATANG DI CLEARMIND CHATBOT====")
-	fmt.Println("APA YANG BISA SAYA BANTU HARI INI?")
-	fmt.Println("1. Produktifitas")
-	fmt.Println("2. Kesehatan Mental")
-	fmt.Scan(&pilih)
-	if pilih == 1 {
-		produktifitas(A)
-	} else if pilih == 2 {
+	fmt.Printf("===== ClearMind =====\n")
+	fmt.Println("[1] Menu Produktivitas")
+	fmt.Println("[2] Menu Kesehatan Mental")
+	fmt.Println("[0] Keluar Progam")
+	fmt.Println("Pilih 1 atau 2: ")
+	
+	var pilihan string
+	fmt.Scan(&pilihan)
+	
+	switch pilihan {
+	case "1":
+			fmt.Print("On Progress")
+	case "2":
+			MenuKesehatanMental()
+	case "0":
+			fmt.Print("Terima Kasih sudah menggunakan ClearMind. Semoga hari Anda menyenangkan.")
+	}
 }
 func produktifitas(A tabtugas) {
 	var pilihan int
@@ -66,3 +75,24 @@ func tugasPrioritas(A tabtugas){
 	}
 }
 
+func MenuKesehatanMental(){
+	fmt.Printf("=== CEK KESEHATAN MENTAL ===\n")
+	fmt.Println("[1] Cek Koin Mental dan Stress Meter")
+	fmt.Println("[2] Isi Jurnal Suasana Hati")
+	fmt.Println("[3] Lihat Stastistik Mingguan")
+	fmt.Println("[0] Kembali ke Menu Utama")
+	
+	var pilihan string
+	fmt.Scan(&pilihan)
+	
+	switch pilihan {
+	case "1":
+			fmt.Print("On Progress")
+	case "2":
+			fmt.Print("On Progress")
+	case "3":
+			fmt.Print("On Progress")
+	case "0":
+			menuUtama()
+	}
+}
